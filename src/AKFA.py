fixed computation of matrix

src/AKFA.py
@@ -13,6 +13,8 @@ import time
 from scipy import sparse
 from scipy.spatial.distance import pdist, squareform
 from scipy import exp
+import h5py as hpy
+from sklearn.metrics.pairwise import pairwise_distances as padis
 # -------------------- Utility Functions -------------------------#
 
 '''
@@ -21,11 +23,6 @@ Used to compute the gaussian kernel of a given matrix
 def gaussian(vec_one,vec_two, sigma=4):
     return math.exp(-np.linalg.norm(vec_one.todense() - vec_two.todense(),2)**2/(2*sigma*sigma))
 
-def sparseNorm2(data):
-    value = 0.0
-    for i in xrange(data.shape[0]):
-        value += data[i]*data[i]
-    return sqrt(value)
 
 '''
 Project components onto new data
@@ -49,55 +46,51 @@ def projectKernelComp(dataset,comp,numberOfDataPoints,numberOfFeatures):
 
 # -------------------- Code -------------------------#
 
-def akfa(dataset, featureNumber=2, delta = 0.0, sigma=4,):
-    #dataset = sparse.dok_matrix(dataset.transpose(),dtype=np.double)
+def akfa(dataset, featureNumber=2, delta = 0.0, sigma=4, chunkSize = 4):
     print("___________")
     print("Setting up for Feature Extraction via Accelerated Kernel Feature Analysis")
     print(" ... %d features are to be extracted!" %(featureNumber))
     print("Data set begin loaded...")
     print(".....")
     print(".....")
-    # first off, we need to check if the given data set is a numpy array or a sparse matrix
-    if( isinstance(dataset,np.ndarray)):
-        print("Data set is not a sparse matrix --> reconfiguring into sparse matrix")
-        dataset = sparse.dok_matrix(dataset,dtype=np.double)
-        print(".....")
-        print("Conversion to sparse matrix successfull")
-    #elif( (not isinstance(dataset,sparse.csr_matrix)) or (not isinstance(dataset,sparse.csc.csc_matrix) )):
-        #raise Exception("The data set is not in the right format")
     print("Loading of data set is completed")
     print(".....")
-    # Here we need to transform the matrix, so that we can access the vectors easier
-    #dataset = dataset.transpose()
     n = dataset.shape[0]
+    numberOfFeatures = dataset.shape[1]
     # Now we can compute the Gram Matrix
-    print("The loaded file contains %d samples points and %d dimensions " % (n, dataset.shape[0]))
+    print("The loaded file contains %d samples points and %d dimensions " % (n,numberOfFeatures ))
     print(".....")
-    K = sparse.dok_matrix( (n,n),dtype = np.double)
+    # Now we need to create a file that holds the Gram Matrix
+    files = hpy.File("akfaData","w")
+    kernelMatrix = files.create_dataset("kernelMat", (n,n) , dtype=np.float32)
     print(".....")
     print("Creating new Sparse Matrix for holding the Gram Matrix")
     print("For a large data set, this may take a while ...")
-    #timeMatrix = time.time()
-    #for i in range(n):
-        #for j in range(n):
-            #K[i,j] = gaussian(dataset[:,i], dataset[:,j],sigma)
-        #print("Done for row [%d]"%(i))
-    #print("It took %f seconds to build the Gram matrix" %(time.time()-timeMatrix))
-    #K = K.tocsr()
-    dist = pdist(dataset.todense(), 'euclidean')
-    pairwise_dists = squareform(dist)
-    K = exp(pairwise_dists / sigma**2)
+    chunk = n/chunkSize
+    print("Chunking data set into %d different sents" % (chunkSize))
+    print(".... now starting to compute %d x %d blocks of Gram Matrix" % (chunk,chunk) )
+    timeBef = time.time()
+    for i in range(chunkSize):
+        for j in range(chunkSize):
+            kernelMatrix[i*chunk:(i+1)*chunk,j*chunk:(j+1)*chunk] = exp( - padis(dataset[i*chunk:(i+1)*chunk,:], dataset[j*chunk:(j+1)*chunk,:]) / 2*sigma*sigma)
+            #tmp = exp( - padis(dataset[i*chunk:(i+1)*chunk,:], dataset[j*chunk:(j+1)*chunk,:]) / 2*sigma*sigma)
+        print("  ... Finished the first %d rows" % ((i+1)*chunk))
+    print("It took %f to compute the Gram matrix" % (time.time()-timeBef))
     print(".....")
-    print("Done computing the Gram Matrix")
+    print("Therefore: Done computing the Gram Matrix")
+    print("WUHU!!")
     print("......")
     print("............")
     print("Will continue with extracting features now!")
+    
+    # -----------------------------------------------------
     #center the matrix in feature space
     # LEAVE THAT OUT FOR NOW
     #N = np.multiply(1./dataset.shape[0],np.ones( (datase(dataset.getrow(i)t.shape[0],dataset.shape[0]) , dtype = np.double) ) 
     #K = np.subtract(dataset,np.dot(N,dataset))
     #K = np.subtract(K,np.dot(dataset,N))
     #K = np.add(K,np.dot(N, np.dot(dataset,N)))
+    # -----------------------------------------------------
     # Stop the time
     timeBefore = time.time()
     # Now we need variables to hold the extracted components
@@ -111,34 +104,47 @@ def akfa(dataset, featureNumber=2, delta = 0.0, sigma=4,):
         # Extract i-th feature using (11)
         maxValue = 0
         maxIn = 0
+        timeIn = time.time()
         print("....")
         for j in range(n):
             sumOf = 0
             # Neglect all cases, where diagonal element is smaller then delta
             # delta set to 0.0 therefore we need to set an smaller and equal then
-            if K[j,j] <= delta:
+            if kernelMatrix[j,j] <= delta:
                 continue
             #sumOf = (1/(n*K[j,j])) * K.getcol(j).multiply(K.getcol(j)).sum()
-            sumOf = (1/(n*K[j,j])) * np.multiply(K[j,:],K[j,:]).sum()
+            sumOf = (1/(n*kernelMatrix[j,j])) * np.multiply(kernelMatrix[j,:],kernelMatrix[j,:]).sum()
             if ( sumOf > maxValue):
                 maxValue = sumOf
                 maxIn = j
         # NEED TO CHECK FOR ZERO VALUES IN DATA
         print("........")
-        idxVectors[i,:] =  K[maxIn,:] / sparseNorm2(K[maxIn,:])
+        idxVectors[i,:] =  kernelMatrix[maxIn,:] / np.linalg.norm(kernelMatrix[maxIn,:])
         idx = maxIn
         print("__Feature found!")
+        print("_____ ... which took %f " % (time.time()-timeIn))
         if i == featureNumber-1:
             continue
-        # Now we must use equation (10) to update the Kernel Matrix K       
-        K_new = sparse.dok_matrix( (n,n),dtype = np.double)
-        print("Updating Gram Matrix!")
-        for j in range(n):
-            for k in range(n):
-                K_new[j,k] = K[j,k] - ((K[j,idx]*K[k,idx])/K[idx,idx])
-        K = K_new.tocsr()
+        idxVec = kernelMatrix[:,idx]
+        print(idxVec.shape)
+        print(idxVec[0])
+        print("Now updating the Gram Matrix")
+        timeBef = time.time()
+        # Now we must use equation (10) to update the Kernel Matrix K
+        for i in range(n):
+            fak = idxVec[i]/idxVec[idx]
+            kernelMatrix[:,i] = np.subtract(kernelMatrix[:,i],idxVec*fak)
+            if ( i % 1000 == 0):
+                print("For first %d it took %f" % (i,time.time()-timeBef))  
+        #K_new = sparse.dok_matrix( (n,n),dtype = np.double)
+        #print("Updating Gram Matrix!")
+        #for j in range(n):
+            #for k in range(n):
+                #K_new[j,k] = K[j,k] - ((K[j,idx]*K[k,idx])/K[idx,idx])
+        #K = K_new.tocsr()
         #K.eliminate_zeros()
-        print("Update successfull - continue!")
+        print("Update successfull - in %f " % (time.time()-timeBef))
+        print(" ------> continue!")
         
     tmpTime = time.time()
     print("__________")

src/Tests.py
@@ -7,7 +7,7 @@ from sklearn.datasets import load_svmlight_file
 from sklearn import svm
 from sklearn.metrics import roc_curve, auc
 import pylab as pl
-#from AKFA import akfa
+from AKFA import akfa
 import numpy as np
 from scipy import sparse
 #import gc
@@ -15,21 +15,51 @@ import Util
 import akfa as feature
 from scipy.spatial.distance import cdist
 import h5py as hpy
+import time
 from scipy.spatial.distance import pdist, squareform
 from scipy import exp
 import scipy as sp
 from scipy import spatial
+from scipy.sparse import bmat
+from scipy.sparse import csr_matrix
+import math
+from sklearn.metrics.pairwise import pairwise_distances as pad
+
+
+
+X_train, y_train = load_svmlight_file("./data/dataset_1/train")
+
+numberOfSamples = X_train.shape[0]
+numberOfFeatures = X_train.shape[1]
+
+print("The read file contains %d samples points and  %d features " % (numberOfSamples, numberOfFeatures))
+
+
+akfa(X_train,4,0.0,4,4)
+
+
+
 '''
 # testing for csr_matrix akfa here
 #===============================================================================
-#tmp = np.array( ( (1,2,3,4,5), (-1,3,-5,8,3) ), np.double)
-#xa = np.array( ( (1,2), (-1,2) ), np.double)
-#print(tmp.shape)
-#print cdist(xa.transpose(),tmp.transpose())
-#exit()
-x = Util.gen_Circle()
-comps = feature.akfa(x,4)
+tmp = np.array( ( (1,2,3,4,5), (-1,3,-5,8,3) ), np.double).transpose()
+xa = np.array( ( (1,2), (-1,2) ), np.double)
+print(tmp)
+x  = cdist(tmp,tmp, 'euclidean')
+print squareform(cdist(tmp,tmp, 'euclidean'))
+print pdist(tmp, 'euclidean')
+print x
+print x[2,0:5]
 exit()
+x = Util.gen_Circle()
+
+
+#K = exp(-squareform(pdist(dataset.transpose(), 'euclidean')) / (2*sigma*sigma))
+#K = exp(-cdist(tmp,tmp, 'euclidean') / (2*sigma*sigma))
+
+
+exit()
+
 pl.figure(0)
 pl.title("Original Data")
 #pl.plot(x[0,:], x[1,:], 'ro',color='blue')
@@ -52,13 +82,47 @@ exit()
 #print(isinstance(x,sparse.csr.csr_matrix))
 #print(isinstance(x,np.ndarray))
 #===============================================================================
-'''
+
+
 X_train, y_train = load_svmlight_file("./data/dataset_1/train")
 
 numberOfSamples = X_train.shape[0]
 numberOfFeatures = X_train.shape[1]
-newSet = X_train[:250,:]
+
 print("The read file contains %d samples points and  %d features " % (numberOfSamples, numberOfFeatures))
+timeBef = time.time()
+sigma = 4
+chunkSize = 4
+chunk = numberOfSamples/chunkSize
+for i in range(chunkSize):
+    for j in range(chunkSize):
+        print("Currently at:")
+        print(i,j)
+        timeIn = time.time()
+        na = exp( - pad(X_train[i*chunk:(i+1)*chunk,:], X_train[j*chunk:(j+1)*chunk,:]) / 2*sigma*sigma)
+        print("It took %f to compute the first chunk Gram matrix - if it worked at all" % (time.time()-timeIn))
+print("It took %f to compute the Gram matrix - if it worked at all" % (time.time()-timeBef))
+print(na[0,0])
+
+exit()
+timeIn = time.time()
+for i in range(100):
+    for j in range(100):
+            xy = X_train[0,:].data.shape[0] + X_train[1,:].data.shape[0] - np.intersect1d(X_train[0,:].indices, X_train[1,:].indices).shape[0]*2
+            erg = math.exp(-math.sqrt(xy)**2/(2*4*4))
+            if i == 0 and j == 0:
+                print(erg)
+print("It took %f to compute the first chunk Gram matrix - if it worked at all" % (time.time()-timeIn))
+print()
+
+print("The Kernel value is:")
+print(math.exp(-np.linalg.norm(X_train[0,:].todense() - X_train[1,:].todense(),2)**2/(2*4*4)))
+timeIn = time.time()
+a = csr_matrix(X_train[0:99,:])
+tmp = exp(-cdist(a.todense(),a.todense(), 'euclidean') / (2*4*4))
+print(tmp[0,0])
+print("It took %f to compute the first chunk Gram matrix - if it worked at all" % (time.time()-timeIn))
+exit()
 
 files = hpy.File("akfaData","w")
 sparseMatrix = files.create_dataset("sparseMat", (numberOfSamples,numberOfFeatures) , dtype=np.byte)
@@ -66,19 +130,29 @@ kernelMatrix = files.create_dataset("kernelMat", (numberOfSamples,numberOfSample
 
 #set = file.create_dataset("sparsedata", data=X_train.todense())
 #dset = file.create_dataset("sparsedata", data=exp(squareform(pdist(X_train, 'euclidean'))))
-numberOfSamples = 250
+print("let the storing begin")
 sigma = 4
-
-for i in range(numberOfSamples):
-    sparseMatrix[i,:] = newSet[i,:].todense()
-    # ( i % 1000 == 0):
+#timeNow = time.time()
+#for i in range(numberOfSamples):
+    #sparseMatrix[i,:] = X_train[i,:].todense()
+    #if ( i % 1000 == 0):
         #print(i/1000)
-print("Done!")
-for i in range(numberOfSamples):
-    for j in range(numberOfSamples):
-        kernelMatrix[i,j] = exp(-spatial.distance.euclidean(sparseMatrix[i],sparseMatrix[j])**2/(2*sigma*sigma))
-    print(i)
-'''
+#print("Done!")
+#print("It took %f to store the data set on the file system" % (time.time()-timeNow))
+print("Creating Kernel Matrix")
+timeNow = time.time()
+for i in range(0,numberOfSamples,100):
+    for j in range(0,numberOfSamples,100):
+        timeIn = time.time()
+        #kernelMatrix[i:(i+10000),j:(j+10000)] = exp(-cdist(sparseMatrix[i:(i+10000),:],sparseMatrix[j:(i+10000),:], 'euclidean') / (2*sigma*sigma))
+        tmp = exp(-cdist(X_train[i:(i+99),:],X_train[j:(j+99),:], 'euclidean') / (2*sigma*sigma))
+        print("It took %f to compute the first chunk Gram matrix - if it worked at all" % (time.time()-timeIn))
+print("It took %f to compute the Gram matrix - if it worked at all" % (time.time()-timeNow))
+print("wait a minute, I am here, so it did work Oo")
+print("WUUUUUUUSA")
+#rgk = feature.akfa(sparseMatrix[i:(i+10000),:], kernelMatrix, 2, 0.1,4)
+exit()
+
 X_train, y_train = load_svmlight_file("./data/dataset_1/train")
 numberOfSamples = X_train.shape[0]
 numberOfFeatures = X_train.shape[1]