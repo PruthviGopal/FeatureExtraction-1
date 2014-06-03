14042f31fd01ea40c3a56fea8510db5dd674615a
Christoph Rauterberg <c.rauterberg@gmx.de>
2014-06-03 05:17:47 +0200

    final changes

--- src/Util.py
+++ src/Util.py
@@ -57,62 +57,11 @@ def center_K(matrix):
     return K
 
 '''
-This function is used to compute the gram matrix of a given matrix
-'''
-def comp_K(matrix, kernel='linear'):
-    #matrix = matrix.transpose()
-    N = matrix.shape[0]
-    K = np.zeros((N,N), np.double)
-    for i in range(N):
-        for j in range(N):
-            if kernel == 'exp':
-                K[i][j] = exponential(matrix[i], matrix[j])
-            elif kernel == 'gauss':
-                K[i][j] = gaussian(matrix[i], matrix[j])
-            else:
-                K[i][j] = linear(matrix[i], matrix[j])
-    #K = center_K(K)
-    return K
-
-'''
 This function is used to compute the matrix N used in the centering process
 '''
 def comp_N(size):
     return np.multiply(1./size,np.ones( (size,size) , dtype = float) )
 
-
-'''
-This function is used to compute the eigenvalues of a given matrix
-'''
-def comp_eigen(K):
-    return np.linalg.eig(K)
-
-
-'''
-Project components onto new data
-'''
-def projectKernelComp(learningset,dataset,comp,kernel='gauss'):
-    #matrix = learningset.transpose()
-    finalData = deque([])
-    #now, we must project the chosen components onto the dataset
-    for x in dataset.transpose():
-        #We have now chosen a point in our dataset which is to be adapted
-        y = copy.copy(x)
-        for index in range(x.shape[0]):
-            tmp = 0
-            for i in range(learningset.transpose().shape[0]):
-                if kernel == 'exp':
-                    tmp += comp[index,i]*exponential(learningset.transpose()[i],x)
-                elif kernel == 'gauss':
-                    tmp += comp[index,i]*gaussian(learningset.transpose()[i],x)
-            y[index] = tmp
-        finalData.append(y)
-    fData = np.zeros((dataset.shape[0],dataset.shape[1]),dtype = np.double)
-    for i in range(dataset.shape[1]):
-        point = finalData.popleft()
-        fData[0][i] = point[0]
-        fData[1][i] = point[1]
-    return fData
 
 # --------------- Kernel-Functions ---------------
 
